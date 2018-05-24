0f03379d984f311ced9b3ac214203fba73eccb69
Mike Donnici <michael@mesa.net.au>
2018-05-24 13:51:39 +1000

    Unmarshal articles

--- pubmed/pubmed.go
+++ pubmed/pubmed.go
@@ -1,115 +1,55 @@
+// Package pubmed fetches articles from Pubmed
 package pubmed
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"time"
-	"strings"
-	"net/url"
 
 	"github.com/pkg/errors"
 )
 
-// SearchURL and query parameters
-const SearchURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&usehistory=y"
-const queryReturnMax = "&retmax=%v"
-const queryStartIndex = "&retstart=%v"
+const BaseURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+const SearchURL = BaseURL + "/esearch.fcgi?db=pubmed&retmode=json&usehistory=y"
+const FetchURL = BaseURL + "/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract"
 const queryBackDays = "&reldate=%v&datetype=pdat"
 const querySearchTerm = "&term=%v"
-
-// FetchURL fetches articles
-const FetchURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&query_key=1&WebEnv=NCID_1_94625809_130.14.18.34_9001_1527039838_862671551_0MetA0_S_MegaStore"
+const queryReturnMax = "&retmax=%v"
+const queryStartIndex = "&retstart=%v"
+const queryKey = "&query_key=%v"
+const queryWebEnv = "&WebEnv=%s"
 
 const defaultBackDays = 7
-const defaultMaxSetSize = 1000
 
-// Search represents a request to the Pubmed esearch endpoint and results in one or more sets of article IDs which
-// can be subsequently used to fetch article summaries.
-type Search struct {
+// Query represents a search request to the Pubmed esearch endpoint, the results of which are stored at Pubmed
+// and subsequently referenced by Key and WebEnv
+type Query struct {
 	BackDays    int
 	Term        string
 	ResultCount int
-	QueryKey    string `json:"querykey"`
+	Key         string `json:"querykey"`
 	WebEnv      string `json:"webenv"`
 }
 
-// PubMedSummary result - each result is indexed by the id of the record requested, even if there is only one.
-// As we can pass multiple ids on the URL to save requests eg 521345,765663,121234,3124256
-// use XMl to fetch this as easier to get to nested data
-type PubMedArticleSet struct {
-	Articles []PubMedArticle `xml:"PubmedArticle"`
-}
-type PubMedArticle struct {
-	ID              int            `xml:"MedlineCitation>PMID"`
-	Title           string         `xml:"MedlineCitation>Article>ArticleTitle"`
-	Abstract        []AbstractText `xml:"MedlineCitation>Article>Abstract>AbstractText"`
-	ArticleIDList   []ArticleID    `xml:"PubmedData>ArticleIdList>ArticleId"`
-	KeywordList     []string       `xml:"MedlineCitation>KeywordList>Keyword"`
-	MeshHeadingList []string       `xml:"MedlineCitation>MeshHeadingList>MeshHeading>DescriptorName"`
-	AuthorList      []Author       `xml:"MedlineCitation>Article>AuthorList>Author"`
-	Journal         string         `xml:"MedlineCitation>Article>Journal>Title"`
-	JournalAbbrev   string         `xml:"MedlineCitation>Article>Journal>ISOAbbreviation"`
-	Volume          string         `xml:"MedlineCitation>Article>Journal>JournalIssue>Volume"`
-	Issue           string         `xml:"MedlineCitation>Article>Journal>JournalIssue>Issue"`
-	Pages           string         `xml:"MedlineCitation>Article>Pagination>MedlinePgn"`
-	PubYear         string         `xml:"MedlineCitation>Article>Journal>JournalIssue>PubDate>Year"`
-	PubMonth        string         `xml:"MedlineCitation>Article>Journal>JournalIssue>PubDate>Month"`
-	PubDay          string         `xml:"MedlineCitation>Article>Journal>JournalIssue>PubDate>Day"`
-
-	// Note that for these fallback dates there are multiple nodes as each is part of the records history
-	// Ideally, we would pick the xml node with the attribute 'entrez', which is the oldest.
-	// today loo into ensuring the oldest date element is selected for fallback
-	PubYearFallback  string `xml:"PubmedData>History>PubMedPubDate>Year"`
-	PubMonthFallback string `xml:"PubmedData>History>PubMedPubDate>Month"`
-	PubDayFallback   string `xml:"PubmedData>History>PubMedPubDate>Day"`
-}
-
-type AbstractText struct {
-	Key   string `xml:"label,attr"`
-	Value string `xml:",chardata"`
-}
-
-type ArticleID struct {
-	Key   string `xml:"IdType,attr"`
-	Value string `xml:",chardata"`
-}
-
-type Author struct {
-	Key      string `xml:"ValidYN,attr"`
-	LastName string `xml:"LastName"`
-	Initials string `xml:"Initials"`
-}
-
-type Resource struct {
-	Published   time.Time              `json:"published" bson:"published"`
-	Title       string                 `json:"title"`
-	Description string                 `json:"description"`
-	Keywords    []string               `json:"keywords"`
-	URL         string                 `json:"url"`
-	Attributes  map[string]interface{} `json:"attributes"`
-}
-
-// NewSearch returns a pointer to a Search with some defaults set
-func NewSearch(query string) *Search {
-	return &Search{
+// NewSearch returns a pointer to a Query with some defaults set
+func NewSearch(query string) *Query {
+	return &Query{
 		BackDays: defaultBackDays,
 		Term:     query,
 	}
 }
 
-//
-func (ps *Search) StoreResults() error {
+// Search executes the query, results are stored at Pubmed and referenced by the Key and WenEnv values
+func (ps *Query) Search() error {
 
-	sURL := SearchURL +
-		fmt.Sprintf(queryBackDays, ps.BackDays) +
-		fmt.Sprintf(querySearchTerm, ps.Term)
-
-	xb, err := ResponseGET(sURL)
+	qURL := SearchURL + fmt.Sprintf(queryBackDays, ps.BackDays) + fmt.Sprintf(querySearchTerm, ps.Term)
+	xb, err := responseBody(qURL)
 	if err != nil {
-		return errors.Wrap(err, "StoreResults")
+		return errors.Wrap(err, "Search")
 	}
 
 	var r = struct {
@@ -119,118 +59,63 @@ func (ps *Search) StoreResults() error {
 			WebEnv   string `json:"webenv"`
 		} `json:"esearchresult"`
 	}{}
-
 	err = json.Unmarshal(xb, &r)
 	if err != nil {
-		return errors.Wrap(err, "StoreResults, Unmarshal")
+		return errors.Wrap(err, "Search, Unmarshal")
 	}
 
 	count, err := strconv.Atoi(r.Result.Count)
 	if err != nil {
-		return errors.Wrap(err, "StoreResults, Atoi")
+		return errors.Wrap(err, "Search, Atoi")
 	}
 	ps.ResultCount = count
-
-	ps.QueryKey = r.Result.QueryKey
+	ps.Key = r.Result.QueryKey
 	ps.WebEnv = r.Result.WebEnv
 
 	return nil
 }
 
-// QueryArticles fetches a set of Pubmed article summaries and unmarshals them into a PubMedSet.
-// There is no limited to the number of article that may be requested however it is recommended to use the POST method
-// for requests of more than 200 articles. The list of article ids is posted as form data in the body of the request.
+// Articles fetches a set of articles from the Pubmed cache referenced by Key and WebEnv. The response is an xml
+// payload that is unmarshaled into a PubMedSet.
 // Ref: https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_EFetch_
-func (ps *Search) QueryArticles(ids ...string) ([]PubMedArticle, error) {
+func (ps *Query) Articles(startIndex, retMax int) (ArticleSet, error) {
 
-	var xpa []PubMedArticle
+	var set ArticleSet
 
-	form := url.Values{}
-	form.Add("ids", strings.Join(ids, ","))
-
-	xb, err := ResponsePOST(FetchURL, form)
+	qURL := FetchURL +
+		fmt.Sprintf(queryKey, ps.Key) +
+		fmt.Sprintf(queryWebEnv, ps.WebEnv) +
+		fmt.Sprintf(queryStartIndex, startIndex) +
+		fmt.Sprintf(queryReturnMax, retMax)
+	xb, err := responseBody(qURL)
 	if err != nil {
-		return xpa, errors.Wrap(err, "QueryArticles could not get response body")
+		return set, errors.Wrap(err, "Articles could not get response body")
 	}
 
-	fmt.Println(string(xb))
+	err = xml.Unmarshal(xb, &set)
+	if err != nil {
+		return set, errors.Wrap(err, "Articles could not unmarshal response body")
+	}
 
-	//resultSet.IDs, err = ResultsSetIDs(xb)
-	//if err != nil {
-	//	return resultSet, errors.Wrap(err, "QueryArticles could not extract count from response body")
-	//}
+	for i, a := range set.Articles {
+		set.Articles[i].PubDate, _ = bestPubDate(a) // todo ... ignore the error?
+		set.Articles[i].Keywords = mergeKeywords(a)
+		set.Articles[i].URL = articleURL(a)
+		set.Articles[i].Citation = citation(a)
+	}
 
-	return nil, nil
+	return set, nil
 }
 
-// ResponseGET returns the response body from a GET request
-func ResponseGET(url string) ([]byte, error) {
+// responseBody returns the response body from a GET request as a []byte
+func responseBody(url string) ([]byte, error) {
 
 	httpClient := &http.Client{Timeout: 90 * time.Second}
-
 	r, err := httpClient.Get(url)
 	if err != nil {
-		return nil, errors.Wrap(err, "ResponseGET")
+		return nil, errors.Wrap(err, "responseBody")
 	}
 	defer r.Body.Close()
 
 	return ioutil.ReadAll(r.Body)
 }
-
-// ResponsePOST returns the response body from a POST request
-func ResponsePOST(url string, data url.Values) ([]byte, error) {
-
-	httpClient := &http.Client{Timeout: 90 * time.Second}
-
-	r, err := httpClient.PostForm(url, data)
-	if err != nil {
-		return nil, errors.Wrap(err, "ResponseGET")
-	}
-	defer r.Body.Close()
-
-	return ioutil.ReadAll(r.Body)
-}
-
-// extract the count value from response body
-func ResultsCount(responseBody []byte) (int, error) {
-
-	var r = struct {
-		Result struct {
-			Count string `json:"count"`
-		} `json:"esearchresult"`
-	}{}
-
-	err := json.Unmarshal(responseBody, &r)
-	if err != nil {
-		return 0, errors.Wrap(err, "Unmarshal")
-	}
-
-	count, err := strconv.Atoi(r.Result.Count)
-	if err != nil {
-		return 0, errors.Wrap(err, "Atoi")
-	}
-
-	return count, nil
-}
-
-// ResultsSetIDs extracts the list of ids from the JSON response body
-func ResultsSetIDs(responseBody []byte) ([]string, error) {
-
-	var r = struct {
-		Result struct {
-			IDList []string `json:"idlist"`
-		} `json:"esearchresult"`
-	}{}
-
-	err := json.Unmarshal(responseBody, &r)
-	if err != nil {
-		return []string{}, errors.Wrap(err, "Unmarshal")
-	}
-
-	return r.Result.IDList, nil
-}
-
-// NumSets calculates the number of sets required for a Search based on MaxSetSize and total results.
-//func (ps *Search) NumSets() int {
-//	return int(math.Ceil(float64(ps.Result.Total) / float64(ps.Result.MaxSetSize)))
-//}
--- pubmed/pubmed_test.go
+++ pubmed/pubmed_test.go
@@ -1,16 +1,15 @@
 package pubmed_test
 
 import (
-	"testing"
 	"io/ioutil"
 	"log"
+	"testing"
 
 	"github.com/8o8/articles/pubmed"
 	"github.com/matryer/is"
-	"fmt"
 )
 
-const query = `loattrfree%20full%20text%5BFilter%5D%20AND%20(%22Am%20Heart%20J%22%5Bjour%5D%20OR%20%22Am%20J%20Cardiol%22%5Bjour%5D%20OR%20%22Arterioscler%20Thromb%20Vasc%20Biol%22%5Bjour%5D%20OR%20%22Atherosclerosis%22%5Bjour%5D%20OR%20%22Basic%20Res%20Cardiol%22%5Bjour%5D%20OR%20%22Cardiovasc%20Res%22%5Bjour%5D%20OR%20%22Chest%22%5Bjour%5D%20OR%20%22Circulation%22%5Bjour%5D%20OR%20%22Circ%20Arrhythm%20Electrophysiol%22%5Bjour%5D%20OR%20%22Circ%20Cardiovasc%20Genet%22%5Bjour%5D%20OR%20%22Circ%20Cardiovasc%20Imaging%22%5Bjour%5D%20OR%20%22Circ%20Cardiovasc%20Qual%20Outcomes%22%5Bjour%5D%20OR%20%22Circ%20Cardiovasc%20Interv%22%5Bjour%5D%20OR%20%22Circ%20Heart%20Fail%22%5Bjour%5D%20OR%20%22Circ%20Res%22%5Bjour%5D%20OR%20%22ESC%20Heart%20Fail%22%5Bjour%5D%20OR%20%22Eur%20Heart%20J%22%5Bjour%5D%20OR%20%22Eur%20Heart%20J%20Cardiovasc%20Imaging%22%5Bjour%5D%20OR%20%22Eur%20Heart%20J%20Acute%20Cardiovasc%20Care%22%5Bjour%5D%20OR%20%22Eur%20Heart%20J%20Cardiovasc%20Pharmacother%22%5Bjour%5D%20OR%20%22Eur%20Heart%20J%20Qual%20Care%20Clin%20Outcomes%22%5Bjour%5D%20OR%20%22Eur%20J%20Heart%20Fail%22%5Bjour%5D%20OR%20%22Eur%20J%20Vasc%20Endovasc%20Surg%22%5Bjour%5D%20OR%20%22Europace%22%5Bjour%5D%20OR%20%22Heart%22%5Bjour%5D%20OR%20%22Heart%20Lung%20Circ%22%5Bjour%5D%20OR%20%22Heart%20Rhythm%22%5Bjour%5D%20OR%20%22JACC%20Cardiovasc%20Interv%22%5Bjour%5D%20OR%20%22JACC%20Cardiovasc%20Imaging%22%5Bjour%5D%20OR%20%22JACC%20Heart%20Fail%22%5Bjour%5D%20OR%20%22J%20Am%20Coll%20Cardiol%22%5Bjour%5D%20OR%20%22J%20Am%20Heart%20Assoc%22%5Bjour%5D%20OR%20%22J%20Am%20Soc%20Echocardiogr%22%5Bjour%5D%20OR%20%22J%20Card%20Fail%22%5Bjour%5D%20OR%20%22J%20Cardiovasc%20Electrophysiol%22%5Bjour%5D%20OR%20%22J%20Cardiovasc%20Magn%20Reson%22%5Bjour%5D%20OR%20%22J%20Heart%20Lung%20Transplant%22%5Bjour%5D%20OR%20%22J%20Hypertens%22%5Bjour%5D%20OR%20%22J%20Mol%20Cell%20Cardiol%22%5Bjour%5D%20OR%20%22J%20Thorac%20Cardiovasc%20Surg%22%5Bjour%5D%20OR%20%22J%20Vasc%20Surg%22%5Bjour%5D%20OR%20%22Nat%20Rev%20Cardiol%22%5Bjour%5D%20OR%20%22Prog%20Cardiovasc%20Dis%22%5Bjour%5D%20OR%20%22Resuscitation%22%5Bjour%5D%20OR%20%22Stroke%22%5Bjour%5D)`
+const query = `asthma`
 
 var mockResponseJSON = map[string][]byte{
 	"count":   []byte{},
@@ -20,7 +19,7 @@ var mockResponseJSON = map[string][]byte{
 }
 
 var mockResponseXML = map[string][]byte{
-	"articles":   []byte{},
+	"articles": []byte{},
 }
 
 func init() {
@@ -42,21 +41,6 @@ func init() {
 		}
 		mockResponseXML[i] = xb
 	}
-}
-
-func TestNumberOfSets(t *testing.T) {
-	var err error
-	is := is.New(t)
-	ps := pubmed.NewSearch(query)
-
-	ps.ResultCount, err = pubmed.ResultsCount(mockResponseJSON["count"])
-	is.NoErr(err)             // Error setting Total
-	//is.Equal(ps.NumSets(), 3) // Expect 3 sets
-
-	//ps.Result.MaxSetSize = 50
-	//ps.Result.Total, err = pubmed.ResultsCount(mockResponseJSON["count"])
-	//is.NoErr(err)              // Error setting Total
-	//is.Equal(ps.NumSets(), 42) // Expect 42 sets
 }
 
 //func TestResultsSetIDs(t *testing.T) {
@@ -89,19 +73,25 @@ func TestRealSearch(t *testing.T) {
 	is := is.New(t)
 	ps := pubmed.NewSearch(query)
 	ps.BackDays = 100
-	err := ps.StoreResults()
-	is.NoErr(err)                // Error running query
-	//is.True(ps.Result.Total > 0) // No results for last 100 days?
-	fmt.Println(ps.ResultCount)
-	fmt.Println(ps.QueryKey)
-	fmt.Println(ps.WebEnv)
+	err := ps.Search()
+	is.NoErr(err)               // Search error
+	is.True(ps.ResultCount > 0) // No results for last 100 days?
+	set, err := ps.Articles(0, 1000)
+	is.NoErr(err) // Fetch error
+
+	for _, a := range set.Articles {
+		if len(a.MeshHeadings) > 0 {
+			a.Print()
+		}
+	}
 }
+
 //
 //func TestFetchArticles(t *testing.T) {
 //	is := is.New(t)
 //	ps := pubmed.NewSearch(query)
 //	ps.Name = "Cardiology"
-//	xps, err := ps.QueryArticles("29735362", "29730991")
+//	xps, err := ps.Articles("29735362", "29730991")
 //	is.NoErr(err) // Error fetching articles
 //	fmt.Println(xps)
 //}
