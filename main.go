18d8061a2783369bcbd3ef5e29fff2c3c289e917
Mike Donnici <michael@mesa.net.au>
2018-05-25 13:45:45 +1000

    move pubmed pkg, test from main

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/mikedonnici/pubmed"
 )
 
 func main() {
 	fmt.Println("Fetching articles from pubmed...")
 
+	p := pubmed.NewSearch("shoulder%20subluxation")
+	p.BackDays = 1000
+	p.Search()
+	xa, err := p.Articles(0, 1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, a := range xa.Articles {
+		a.Print()
+	}
 }
